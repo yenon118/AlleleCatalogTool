65a0ac6ea1958ff79751be16b488d75a9dc1ac0c
Yen On Chan <yenon118@gmail.com>
2022-02-06 00:42:25 -0600

    Change config to routes

--- code_collector.sh
+++ code_collector.sh
@@ -4,4 +4,4 @@ cp -r /data/html/Prod/KBCommons_multi/public/system/home/AlleleCatalogTool/css /
 
 cp -r /data/html/Prod/KBCommons_multi/public/system/home/AlleleCatalogTool/js /data/html/Prod/KBCommons_multi/resources/views/system/tools/AlleleCatalogTool/
 
-grep -e "Allele Catalog" -e "AlleleCatalog" /data/html/Prod/KBCommons_multi/routes/web.php > /data/html/Prod/KBCommons_multi/resources/views/system/tools/AlleleCatalogTool/config/web.php
+grep -e "Allele Catalog" -e "AlleleCatalog" /data/html/Prod/KBCommons_multi/routes/web.php > /data/html/Prod/KBCommons_multi/resources/views/system/tools/AlleleCatalogTool/routes/web.php
